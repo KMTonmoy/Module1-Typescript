0dcfd357b74bdccfa5c007181f790d60c6e59a7d
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-10-31 22:00:14 +0600

    Problem Solved

---
+++ module1/src/1.10.ts
@@ -0,0 +1,29 @@
+{
+  // *
+
+  type FontendDeveloper = "fakibazdeveloper" | "juniordeveloper";
+  type FullStackDeveloper = "FontendDeveloper" | "ExpertDeveloper";
+
+  type Developer = FontendDeveloper | FullStackDeveloper;
+
+  type User = {
+    name: string;
+    email: string;
+    gender: "male" | "femail";
+    bloodGroup: "O+" | "A+" | "B-";
+  };
+
+  const user1: user = {
+    name: "Tonmoy",
+    gender: "male",
+    bloodGroup: "O+",
+  };
+
+    
+    
+
+    
+    
+    
+  // *
+}
---
+++ module1/src/1.9.ts
@@ -0,0 +1,25 @@
+{
+  // *
+
+  type student = {
+    name: string;
+    age: number;
+    gender: string;
+    address: string;
+  };
+
+  const student1: student = {
+    name: "Tonmoy",
+    gender: "male",
+    age: 14,
+    address: "Pabna",
+  };
+  const student2: student = {
+    name: "Toma",
+    gender: "femail",
+    age: 22,
+    address: "Pabna",
+  };
+
+  // *
+}
